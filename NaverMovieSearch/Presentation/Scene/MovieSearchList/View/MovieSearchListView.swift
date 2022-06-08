0283faf0f9d657526a721307478daa23783be989
HJEHA <hyhpwang4@gmail.com>
2022-06-08 22:51:18 +0900

    feat: 즐겨찾기 버튼 추가 #6

--- NaverMovieSearch/Presentation/Scene/MovieSearchList/View/MovieSearchListView.swift
+++ NaverMovieSearch/Presentation/Scene/MovieSearchList/View/MovieSearchListView.swift
@@ -17,6 +17,23 @@ final class MovieSearchListView: UIView {
         return label
     }()
     
+    let favoriteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("즐겨찾기", for: .normal)
+        button.setTitle("즐겨찾기", for: .selected)
+        button.setTitleColor(.label, for: .normal)
+        button.setTitleColor(.label, for: .selected)
+        button.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        button.setImage(UIImage(systemName: "star.fill"), for: .selected)
+        button.tintColor = .systemYellow
+        button.contentEdgeInsets = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemGray.cgColor
+        button.translatesAutoresizingMaskIntoConstraints =  false
+        
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
@@ -41,12 +58,19 @@ final class MovieSearchListView: UIView {
     
     private func configureSubviews() {
         addSubview(titleLabel)
+        addSubview(favoriteButton)
     }
     
     private func configureSubViewsConstraint() {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            favoriteButton.heightAnchor.constraint(equalTo: titleLabel.heightAnchor),
+            favoriteButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            favoriteButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16)
         ])
     }
 }
