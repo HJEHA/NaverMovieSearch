dfa6aaa603907f60f4bfba9f1a2e0c64f4f9b2a5
HJEHA <hyhpwang4@gmail.com>
2022-06-08 21:25:02 +0900

    feat: main stroyboard 제거 #6

--- NaverMovieSearch/Presentation/Scene/MovieSearchList/View/MovieSearchListViewController.swift
+++ NaverMovieSearch/Presentation/Scene/MovieSearchList/View/MovieSearchListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MovieSearchListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
